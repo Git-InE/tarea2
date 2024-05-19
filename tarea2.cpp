64c7c319da83c04aaedaa0ab553aa109afc641ec
Git-InE <briosl@usm.cl>
2024-05-19 18:20:41 -0400

    a

--- tarea2.cpp
+++ tarea2.cpp
@@ -1,68 +1,36 @@
-using namespace std;
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include "super_string.c++"
+using namespace std;
 
-class super_string {
-    private:
-        struct nodo {
-            nodo *left = nullptr, *right = nullptr;
-            int index;
-            char c;
-            nodo(int index, char c) {}
-            };
-        int height = 0; // Altura del árbol
-        int length = 0; // Largo del super-string
-        nodo* root = nullptr; // Raíz del super-string
-        void super_string::limpiarRecursivo(nodo*& nodoActual); //nuevo
-    public:
-        super_string() {}
-        void juntar(super_string &s);
-        void agregar(char c); // Insertar un caracter en la última posición
-            // En la izquierda esta el super_string a y en la derecha el super_string b
-        void separar(int i, super_string &a, super_string &b);
-        void reverso(); // No debe cambiar la altura del árbol
-        int recortar(); // Retorna this->height después de recortar
-        string stringizar(); // Debe ser O(n)
-        void limpiar(); // Se deben borrar todos los nodos del super-string
-};
-
-
-void super_string::juntar(super_string &s) {
-
-}
-void super_string::agregar(char c){
-
-}
-
-void super_string::separar(int i, super_string &a, super_string &b){
-
-}
-
-void super_string::reverso(){
-
-}
-
-int super_string::recortar(){
-
-}
-
-string super_string::stringizar(){
-
-}
-/// provisional
-void super_string::limpiar() {
-    limpiarRecursivo(root);
-    root = nullptr;
-    height = 0;
-    length = 0;
-}
-
-void super_string::limpiarRecursivo(nodo*& nodoActual){
-    if (nodoActual != nullptr) {
-        limpiarRecursivo(nodoActual->left);
-        limpiarRecursivo(nodoActual->right);
-        delete nodoActual;
-        nodoActual = nullptr;
-    }
-return;
+int main(){
+    ifstream archivo;
+    string linea;
+    super_string ss;
+	archivo.open("prueba.txt");
+    if (!archivo.is_open()){
+        cerr << "Error al abrir el archivo." << endl;
+        return 1;
+	}
+    ss.agregar('h');
+    ss.agregar('o');
+    ss.agregar('l');
+    ss.agregar('a');
+    while (getline(archivo, linea)){
+        istringstream iss(linea);
+        string operacion;
+        iss >> operacion;
+        if (operacion == "INSERTAR"){
+            int i;
+            string s;
+            iss >> i >> s;
+        }
+        if (operacion == "MOSTRAR"){
+            ss.stringizar();
+            
+        }
+        }
+    archivo.close();
 }
